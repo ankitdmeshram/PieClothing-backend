60eb40127d07d3559562395de29107df80c76000
Ankit Meshram <ankitdm69@gmail.com>
2023-11-15 11:18:51 +0530

    view cart and add to cart updated

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -5,27 +5,68 @@ exports.addToCart = async (req, res) => {
     const { pid, uid, tempUid, size, color, created_date, updated_date } =
       req.body;
 
-    const CartDetails = await Cart.create({
-      products: [
+    const cartOne = await Cart.find({ uid: uid });
+
+    console.log("cart = ", cartOne, cartOne[0]?.products, typeof cartOne);
+    let CartDetails;
+    if (cartOne.length > 0) {
+      cartOne[0]?.products.push({
+        pid: pid,
+        size: size,
+        color: color,
+      });
+
+      CartDetails = await Cart.updateOne(
+        { uid: uid },
         {
-          pid: pid,
-          size: size,
-          color: color,
-        },
-      ],
-      uid: uid,
-      tempUid: tempUid,
-    });
+          $set: {
+            products: cartOne[0]?.products,
+          },
+        }
+      );
+    } else {
+      CartDetails = await Cart.create({
+        products: [
+          {
+            pid: pid,
+            size: size,
+            color: color,
+          },
+        ],
+        uid: uid,
+        tempUid: tempUid,
+      });
+    }
+
     console.log(CartDetails);
     return res.status(200).json({
       success: true,
       message: "Added To Cart Successfully",
       CartDetails,
+      cartOne,
     });
   } catch (error) {
     return res.status(500).json({
-      success: true,
+      success: false,
       message: error.message,
+    });
+  }
+};
+
+exports.viewCartById = async (req, res) => {
+  try {
+    const { uid } = req.body;
+    const cartOne = await Cart.find({ uid: uid });
+    console.log("cart", cartOne);
+    return res.status(200).json({
+      success: true,
+      message: "Cart found successfully",
+      cartOne,
+    });
+  } catch (err) {
+    return res.status(504).json({
+      success: false,
+      message: "Something went wrong",
     });
   }
 };
--- route/Cart.js
+++ route/Cart.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { addToCart } = require("../controllers/CartController");
+const { addToCart, viewCartById } = require("../controllers/CartController");
 
 router.post("/addtocart", addToCart);
+router.post("/viewcart", viewCartById);
 
 module.exports = router;
